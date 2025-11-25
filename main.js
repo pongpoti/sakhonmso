7e86f180c0f07875490b3909a37a3963692bc708
pongpoti <pong.poti@gmail.com>
2025-11-25 10:57:38 +0700

    c

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const path = require("node:path");
+import express from "express";
+import path from "node:path";
+import process from "node:process";
 
 const app = express();
 const port = process.env.PORT || 3030;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(import.meta.dirname, "public")));
 app.use("/external", express.static("external"));
 
-app.get("/get", (req, res) => {
-    res.send("hi there")
-})
+app.get("/get", (_, res) => {
+  res.send("hi there");
+});
 
 app.listen(port, () => {
-    console.log(".. 3030 ..");
+  console.log(".. 3030 ..");
 });
