ecb2e640898fc4f33f3b3187523cbc415a9d960d
pongpoti <pong.poti@gmail.com>
2025-11-25 10:16:34 +0700

    c

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3030;
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/external", express.static("external"));
 
+app.get("/get", (req, res) => {
+    res.send("hi there")
+})
+
 app.listen(port, () => {
     console.log(".. 3030 ..");
 });
