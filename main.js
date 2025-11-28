6e8d363345049fa2fd3d85eb936059635139e779
pongpoti <pong.poti@gmail.com>
2025-11-28 09:17:07 +0700

    c

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import express from "express";
-//import path from "node:path";
 import process from "node:process";
+import * as line from "@line/bot-sdk";
+import { rmSync } from "node:fs";
 
 const app = express();
 const port = process.env.PORT || 3030;
-
-//app.use(express.static(path.join(import.meta.dirname, "public")));
-app.use("/status", express.static("status"));
-app.use("/external", express.static("external"));
-
-app.listen(port, () => {
-  console.log(".. 3030 ..");
+const config = {
+  channelSecret: "fbd3ebe2ca4028a0af48b9b65ed870fa",
+};
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken:
+    "CfooTgPWPH5iXEy9g0a8yGbzFXqMEchsXbW35GI0SQmtYLJqQHX45FwqBuorTlUqlxvg817UnB9vcpWDykEs8gdJdtf97k0fvTWQigrdRCOMq4J4IZ1RTvrV+CE8Q61D92LyyNQ98sCdmdXBMZ+jgAdB04t89/1O/w1cDnyilFU=",
 });
 
 const _color = [
@@ -27,3 +27,45 @@ const _color = [
   "bg-violet-300",
   "bg-fuchsia-300",
 ];
+
+app.use("/status", express.static("status"));
+app.use("/external", express.static("external"));
+
+app.listen(port, () => {
+  console.log(".. 3030 ..");
+});
+
+app.post("/line", line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((error) => console.error(error));
+});
+
+function handleEvent(event) {
+  const data = event.postback.data;
+  const userId = event.source.userId;
+  if (data === "rm_status") {
+    console.log("rm_status");
+  } else if (data === "rm_send") {
+    client.pushMessage({
+      "to": userId,
+      "messages": [
+        {
+          "type": "text",
+          "text": "ส่วนนี้ยังไม่เปิดใช้งาน",
+        },
+      ],
+    });
+  } else if (data === "rm_sign") {
+    client.pushMessage({
+      "to": userId,
+      "messages": [
+        {
+          "type": "text",
+          "text": "ส่วนนี้ยังไม่เปิดใช้งาน",
+        },
+      ],
+    });
+  }
+}
