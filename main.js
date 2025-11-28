ff0759c3f222f2e63cf3bb3d0cdc3016952d4dbc
pongpoti <pong.poti@gmail.com>
2025-11-28 20:55:39 +0700

    c

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const client = new line.messagingApi.MessagingApiClient({
     "CfooTgPWPH5iXEy9g0a8yGbzFXqMEchsXbW35GI0SQmtYLJqQHX45FwqBuorTlUqlxvg817UnB9vcpWDykEs8gdJdtf97k0fvTWQigrdRCOMq4J4IZ1RTvrV+CE8Q61D92LyyNQ98sCdmdXBMZ+jgAdB04t89/1O/w1cDnyilFU=",
 });
 
-const _color = [
+const color_array = [
   ["bg-red-300", "#ffa2a2"],
   ["bg-orange-300", "#ffb86a"],
   ["bg-yellow-300", "#ffdf20"],
@@ -32,6 +32,36 @@ const _color = [
   ["bg-indigo-300", "#a3b3ff"],
   ["bg-violet-300", "#c4b4ff"],
   ["bg-fuchsia-300", "#f4a8ff"],
+];
+
+const month_array = [
+  "มกราคม",
+  "กุมภาพันธ์",
+  "มีนาคม",
+  "เมษายน",
+  "พฤษภาคม",
+  "มิถุนายน",
+  "กรกฎาคม",
+  "สิงหาคม",
+  "กันยายน",
+  "ตุลาคม",
+  "พฤศจิกายน",
+  "ธันวาคม",
+];
+
+const month_iterator = [
+  [[11, -1], [10, -1], [9, -1]],
+  [[0, 0], [11, -1], [10, -1]],
+  [[1, 0], [0, 0], [11, -1]],
+  [[2, 0], [1, 0], [0, 0]],
+  [[3, 0], [2, 0], [1, 0]],
+  [[4, 0], [3, 0], [2, 0]],
+  [[5, 0], [4, 0], [3, 0]],
+  [[6, 0], [5, 0], [4, 0]],
+  [[7, 0], [6, 0], [5, 0]],
+  [[8, 0], [7, 0], [6, 0]],
+  [[9, 0], [8, 0], [7, 0]],
+  [[10, 0], [9, 0], [8, 0]],
 ];
 
 app.use("/status", express.static("status"));
@@ -53,7 +83,16 @@ function handleEvent(event) {
   const userId = event.source.userId;
   loadAnimation(userId);
   if (data === "rm_status") {
-    console.log("rm_status");
+    client.pushMessage({
+      "to": userId,
+      "messages": [
+        {
+          "type": "flex",
+          "altText": "เลือกเดือนที่ต้องการ",
+          "contents": assembleFullList()
+        }
+      ]
+    })
   } else if (data === "rm_send") {
     client.pushMessage({
       "to": userId,
@@ -90,4 +129,122 @@ function loadAnimation(userId) {
   )
     .then()
     .catch((error) => console.error(error));
+}
+
+function createMonthList(i) {
+  const month = (new Date()).getMonth();
+  const year = (new Date()).getFullYear() + 543;
+  const iterator = month_iterator[month];
+  const color = color_array[month][1];
+  const name = month_array(iterator[i][0]) + " " + (year + iterator[i][1]);
+  const postback = (year + iterator[i][1]) + "_" + (iterator[i][0] + 1);
+  const object = {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": name,
+            "align": "center",
+            "size": "lg",
+            "style": "italic",
+            "weight": "bold",
+          },
+        ],
+        "backgroundColor": color,
+        "cornerRadius": "sm",
+        "borderColor": color,
+        "borderWidth": "semi-bold",
+        "flex": 3,
+        "paddingAll": "md",
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "คลิก",
+            "align": "center",
+            "weight": "bold",
+            "size": "lg",
+            "color": "#312c85",
+          },
+        ],
+        "backgroundColor": "#f5f5f5",
+        "cornerRadius": "md",
+        "offsetEnd": "md",
+        "borderColor": "#312c85",
+        "borderWidth": "semi-bold",
+        "flex": 1,
+        "paddingAll": "md",
+        "action": {
+          "type": "postback",
+          "label": postback,
+          "data": postback,
+        },
+      },
+    ],
+    "paddingBottom": "xxl",
+    "paddingTop": "xxl",
+  };
+  return object;
+}
+
+function assembleFullList() {
+  const object = {
+    "type": "bubble",
+    "size": "mega",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "text",
+              "text": "กรุณาเลือกเดือน",
+              "align": "center",
+              "color": "#FFFFFF",
+              "size": "xxl",
+              "margin": "md",
+              "offsetBottom": "sm",
+              "weight": "bold",
+            },
+            {
+              "type": "text",
+              "text": "สามารถดูได้ 3 เดือนย้อนหลัง",
+              "color": "#ffffa0",
+              "align": "center",
+            },
+          ],
+        },
+      ],
+      "backgroundColor": "#1F627C",
+      "paddingAll": "xxl",
+    },
+    "hero": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [],
+      "height": "5px",
+      "backgroundColor": "#81A7AE",
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        createMonthList(0),
+        createMonthList(1),
+        createMonthList(2),
+      ],
+    },
+  };
+  return object;
 }
